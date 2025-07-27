b9799ac52dd11f009526d0189b11f74b11e47328
placeholder09 <gaming4life123921@gmail.com>
2025-07-27 11:04:05 -0400

    Set up basic web project

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <p>
+        Edit <code>main.js</code> and save to test HMR
+      </p>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
